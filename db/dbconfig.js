55cd0c595c3fdd687a189ccf3e1f35c09053fe80
csterwa <chris.sterling@ctl.io>
2015-08-14 12:19:44 -0700

    Updated to use CTL MySQL DBaaS first

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -7,15 +7,29 @@ var connectionInfo = {
 
 if (process.env.VCAP_SERVICES) {
   var services = JSON.parse(process.env.VCAP_SERVICES);
-  var mysqlConfig = services["p-mysql"];
-  if (mysqlConfig) {
-    var node = mysqlConfig[0];
+  var ctlMysqlConfig = services["ctl_mysql"];
+  var pMysqlConfig = services["p-mysql"];
+
+  if (ctlMysqlConfig) {
+    var node = ctlMysqlConfig[0];
     connectionInfo = {
-        host: node.credentials.hostname,
+        host: node.credentials.host,
         port: node.credentials.port,
         user: node.credentials.username,
         password: node.credentials.password,
-        database: node.credentials.name
+        database: node.credentials.dbname,
+        ssl: {
+          ca: node.credentials.certificate
+        }
+    };
+  } else if (pMysqlConfig) {
+    var node = pMysqlConfig[0];
+    connectionInfo = {
+      host: node.credentials.hostname,
+      port: node.credentials.port,
+      user: node.credentials.username,
+      password: node.credentials.password,
+      database: node.credentials.name
     };
   }
 }
